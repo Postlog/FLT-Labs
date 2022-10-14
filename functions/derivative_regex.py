f986187b76578fd5b79b1f4a9652cdfbd1241eb7
pollykon <pollykon@yandex.ru>
2022-10-14 21:28:37 +0300

    Derivative Brzozovski and tree to regex

--- functions/derivative_regex.py
+++ functions/derivative_regex.py
@@ -1,74 +1,127 @@
-class Derivative:
-    result = {
-        "type": "",
-        "first_arg": "",
-        "second_arg": "",
-    }
+import copy
 
-    def __init__(self, differential):
+from models import Regex
+
+
+class DerivativeBrzozowski:
+
+    def __init__(self, differential: int) -> Regex:
         self.differential = differential
 
-    def get_type(self, regex, arg_index):
+    def get_type(self, regex: Regex):
         if regex["type"] == "operation":
             self.check_operation(regex)
         elif regex["type"] == "symbol":
-            if "second_arg" in regex:
-                self.check_var(regex, arg_index)
+            self.check_var(regex)
+        if regex == {}:
+            regex["type"] = "empty_set"
+        return regex
+
+    def check_operation(self, regex: Regex):
+        first_arg = "first_arg"
+        second_arg = "second_arg"
+        operation = "operation"
+        concat = "concat"
+        value = "value"
+        empty_set = "empty_set"
+        symbol = "symbol"
+
+        if regex[operation] == "*":
+            copied_regex = copy.deepcopy(regex)
+            regex[second_arg] = copied_regex
+            regex[operation] = concat
+            regex[first_arg] = self.get_type(regex[first_arg])
+            if regex[first_arg]["type"] == symbol and regex[first_arg][value] == "":
+                regex["type"] = operation
+                regex[operation] = "*"
+                regex[first_arg] = copied_regex[first_arg]
+            elif regex[first_arg]["type"] == empty_set:
+                regex["type"] = empty_set
+
+        elif regex[operation] == "|":
+            regex[first_arg] = self.get_type(regex[first_arg])
+            regex[second_arg] = self.get_type(regex[second_arg])
+            if regex[first_arg]["type"] == empty_set:
+                self.delete_operation(second_arg, regex)
+            elif regex[second_arg]["type"] == empty_set:
+                self.delete_operation(first_arg, regex)
+
+        elif regex[operation] == concat:
+            copied_regex = copy.deepcopy(regex)
+            copied_regex_second_arg = copy.deepcopy(regex[second_arg])
+            copied_regex_first_arg = copy.deepcopy(regex[first_arg])
+            regex[operation] = "|"
+            regex[first_arg] = {
+                "type": "",
+                operation: "",
+                first_arg: {},
+                second_arg: {},
+            }
+            regex[first_arg]["type"] = operation
+            regex[first_arg][operation] = concat
+            regex[first_arg][first_arg] = self.get_type(copied_regex[first_arg])
+            regex[first_arg][second_arg] = copied_regex_second_arg
+            if regex[first_arg][first_arg]["type"] == empty_set:
+                self.delete_key(second_arg, regex)
+            if (copied_regex_first_arg["type"] == "symbol" and copied_regex_first_arg[value] == "") \
+                    or (copied_regex["type"] == operation and copied_regex[operation] == "*"):
+                regex[second_arg] = self.get_type(regex[second_arg])
+                if regex[second_arg]["type"] == empty_set:
+                    self.delete_key(first_arg, regex)
             else:
-                self.check_var(regex, arg_index)
-        return self.result
+                regex[second_arg]["type"] = "empty_set"
+                self.delete_key(first_arg, regex)
+            if regex[first_arg]["type"] == symbol and regex[first_arg][value] == "":
+                self.delete_operation(second_arg, regex)
+                if first_arg in copied_regex_second_arg:
+                    regex[first_arg] = copied_regex_second_arg[first_arg]
 
-    def check_operation(self, regex):
-        self.result = {
-            "type": "operation",
-            "operation": "",
-            "first_arg": "",
-            "second_arg": ""
-        }
-        if regex["operation"] == "*":
-            arg_index = 1
-            self.result["operation"] = "concat"
-            self.get_type(regex["first_arg"], arg_index)
-            self.result["second_arg"] = regex
-        elif regex["operation"] == "|":
-            arg_index = 1
-            self.get_type(regex["first_arg"], arg_index)
-            self.result["operation"] = "|"
-            arg_index = 2
-            self.get_type(regex["second_arg"], arg_index)
-        elif regex["operation"] == "concat":
-            arg_index = 1
-            self.get_type(regex["first_arg"], arg_index)
-            # self.result["first_arg"] = regex["first_arg"]
-            self.result["second_arg"] = regex["second_arg"]
-            self.result["type"] = "|"
-            self.derivative_concat(regex["first_arg"], regex["second_arg"])
+    def delete_operation(self, num_of_arg: str, regex: Regex):
+        operation = "operation"
+        value = "value"
+        symbol = "symbol"
+        copied_regex_dev_first_arg = copy.deepcopy(regex[num_of_arg])
+        regex["type"] = copied_regex_dev_first_arg["type"]
+        if regex["type"] == operation:
+            regex[operation] = copied_regex_dev_first_arg[operation]
+        elif regex["type"] == symbol:
+            regex[value] = copied_regex_dev_first_arg[value]
 
-    def check_var(self, var, arg_index):
+    def delete_key(self, num_of_arg: str, regex: Regex):
+        first_arg = "first_arg"
+        second_arg = "second_arg"
+        copied_regex_dev_first_arg = copy.deepcopy(regex[num_of_arg])
+        self.delete_operation(num_of_arg, regex)
+        regex[num_of_arg] = copied_regex_dev_first_arg[num_of_arg]
+        if second_arg in copied_regex_dev_first_arg:
+            regex[second_arg] = copied_regex_dev_first_arg[second_arg]
+        if first_arg in copied_regex_dev_first_arg:
+            regex[first_arg] = copied_regex_dev_first_arg[first_arg]
+
+    def check_var(self, var: Regex):
         if len(var["value"]) == 1:
-            self.derivative_var(var, arg_index)
+            self.derivative_var(var)
 
-    def derivative_var(self, var, arg_index):
-        num_of_arg = ""
-        if arg_index == 1:
-            num_of_arg = "first_arg"
-        elif arg_index == 2:
-            num_of_arg = "second_arg"
+    def derivative_var(self, var: Regex):
+        value = "value"
+        if var[value] == self.differential:
+            var[value] = ""
+        elif var[value] != self.differential or var[value] == "":
+            var["type"] = "empty_set"
+            del var[value]
 
-        if var["value"] == self.differential:
-            self.result[num_of_arg] = {}
-            self.result[num_of_arg]["type"] = "symbol"
-            self.result[num_of_arg]["value"] = ""
-        elif var == "" or var != self.differential:
-            self.result[num_of_arg] = {}
-            self.result[num_of_arg]["type"] = "empty_set"
 
-    def derivative_concat(self, first_arg, second_arg):
-        if first_arg["type"] == "symbol" and first_arg["value"] == "":
-            arg_index = 2
-            self.get_type(second_arg, arg_index)
-        else:
-            del self.result["operation"]
-            del self.result["type"]
-            del self.result["second_arg"]
-
+def tree_to_regex(tree: Regex) -> str:
+    if tree['type'] == 'empty_set':
+        return "_EMPTYSET_"
+    elif tree['type'] == 'symbol':
+        if tree['value'] == '':
+            return '_EPSILON_'
+        return tree['value']
+    elif tree['type'] == 'operation':
+        if tree['operation'] == "concat":
+            return tree_to_regex(tree['first_arg']) + tree_to_regex(tree['second_arg'])
+        elif tree['operation'] == "*":
+            return f"({tree_to_regex(tree['first_arg'])})*"
+        elif tree['operation'] == "|":
+            return f"({tree_to_regex(tree['first_arg'])}|{tree_to_regex(tree['second_arg'])})"

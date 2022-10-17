2d0f01f5cca5ca78d6da130f541e0c044292366c
Mikhail Kozochkin <mskozochkin@avito.ru>
2022-10-18 01:03:24 +0300

    Переработка системы функций и реализация модели Regex

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,13 +1,25 @@
-from .fibonacci import FibonacciFunction
-from .test import TestFunction
+import typing
 
-functions = {
-    'Fibonacci': FibonacciFunction(),
-}
+from .fibonacci import fibonacci
+from .test import test
+import functions.registry as registry
 
-predicates = {
-}
 
-extras = {
-    "Test": TestFunction()
-}
+def get_types(func: callable) -> tuple[list, list]:
+    hints = typing.get_type_hints(func)
+
+    input_types, output_types = [], []
+    for key, value in hints.items():
+        if key == 'return':
+            if hasattr(value, '__args__'):
+                output_types = list(value.__args__)
+            else:
+                output_types = [value]
+            continue
+
+        if hasattr(value, '__args__'):
+            input_types.append(list(value.__args__))
+        else:
+            input_types.append(value)
+
+    return input_types, output_types

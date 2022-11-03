86a029ee3d9ddb7b58a3915734f7aa49c256d845
pollykon <pollykon@yandex.ru>
2022-11-03 12:13:32 +0300

    Add pumping length

--- functions/pumping_length.py
+++ functions/pumping_length.py
@@ -1,57 +1,66 @@
 from itertools import combinations
 
 from models.regex import Node, NodeType, Regex, RegexParser
-from utils.derivative_utils import DerivativeBrzozovski
 from models.fa import FiniteAutomaton
+from models.int import Int
 from thompson import Thompson
+from utils.derivative_utils import DerivativeBrzozovski, tree_to_regex
+# import functions.registry as registry
 
-def count_pump_length(regex: Regex):
-    automata = Thompson(regex)
-    i = 0
-    pumped_prefixies = set()
-    # for i in range(len(regex_list)):
-    while i < 100:
-        # строим префикс
-        i += 1
-        prefixes = FiniteAutomaton.prefix(automata, i)
+
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub)
+
+
+# @registry.register(registry.FunctionType.REGULAR)
+def PumpingLength(regex: str) -> Int:
+    regex_for_thompson = Regex(regex)
+    nfa = Thompson(regex_for_thompson)
+    dfa = minimize(nfa)
+    parsed_regex = RegexParser.parse(regex)
+    orig_tree = parsed_regex
+    n = 0
+    infix_left = ""
+    pumped_flag = False
+    pumping_prefixes = set()
+    while n < 100:
+        prefixes = FiniteAutomaton.prefix(dfa, n)
+        all_prefixes_pumped = True
         for prefix in prefixes:
+            tree = orig_tree
             for symbol in prefix:
-                # берем производную по префиксу
                 derivative_brzozovski = DerivativeBrzozovski(symbol)
-                derivative_reg_with_prefix = derivative_brzozovski.get_derivative(regex)
+                derivative_reg_with_prefix = derivative_brzozovski.get_derivative(tree)
                 derivative_reg_with_prefix_regex = tree_to_regex(derivative_reg_with_prefix)
-            # перебираем все инфиксы
-            infixes = combinations(prefix, i)
-            infixes_list = ([''.join(i) for i in infixes])
+                tree = derivative_reg_with_prefix
+            comb = combinations(prefix, n)
+            infixes_list = set([''.join(i) for i in comb])
             for infix in infixes_list:
                 if infix != prefix:
                     infix_left = prefix.replace(infix, "", 1)
                 if infix_left != prefix:
-                    index = prefix.index(infix)
-                    pumping_part = "(" + infix + ")" + "*"
-                    pumping_regex = prefix
-                    pumping_regex_inf = pumping_regex.replace(infix, "", 1)
-                    pumping_regex = pumping_regex_inf[:index] + pumping_part + pumping_regex_inf[index:]
-                    regex_to_check = pumping_regex + derivative_reg_with_prefix_regex
-                    # проверяем входят ли они в наш язык
-                    if regex_to_check not in pumped_prefixies:
-                        sub_set_flag = ksubset(regex_to_check, regex)
-                        if sub_set_flag:
-                            pumped_prefixies.add(regex_to_check)
-                            return i
-
-
-def tree_to_regex(tree: Node) -> str:
-    if tree.node_type == NodeType.EMPTY_SET:
-        return "_EMPTYSET_"
-    elif tree.node_type == NodeType.SYMBOL:
-        if tree.value == '':
-            return '_EPSILON_'
-        return tree.value
-    else:
-        if tree.node_type == NodeType.CONCAT:
-            return tree_to_regex(tree.children[0]) + tree_to_regex(tree.children[1])
-        elif tree.node_type == NodeType.ZERO_OR_MORE:
-            return f"({tree_to_regex(tree.children[0])})*"
-        elif tree.node_type == NodeType.ALT:
-            return f"({tree_to_regex(tree.children[0])}|{tree_to_regex(tree.children[1])})"
+                    indexes = list(find_all(prefix, infix))
+                    for index in indexes:
+                        pumping_part = "(" + infix + ")" + "*"
+                        pumping_regex = prefix
+                        pumping_regex_inf = pumping_regex.replace(infix, "", 1)
+                        pumping_regex = pumping_regex_inf[:index] + pumping_part + pumping_regex_inf[len(infix) + index:]
+                        regex_to_check = pumping_regex + derivative_reg_with_prefix_regex
+                        for pumping_prefix in  pumping_prefixes:
+                            if regex_to_check.startswith(pumping_prefix):
+                                pumped_flag = True
+                            else:
+                                pumped_flag = ksubset(regex_to_check, parsed_regex)
+                        if pumped_flag:
+                            pumping_prefixes.add(prefix)
+                        else:
+                            all_prefixes_pumped = False
+                if all_prefixes_pumped:
+                    return n
+                else:
+                    n += 1

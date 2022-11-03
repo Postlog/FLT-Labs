e3a690fb651c219b5d278f3f2a6017a08389f367
pear2jam <71353007+pear2jam@users.noreply.github.com>
2022-11-03 16:35:44 +0300

    Update fibonacci.py

--- functions/fibonacci.py
+++ functions/fibonacci.py
@@ -13,17 +13,3 @@ def fibonacci(n: Int) -> Int:
 
     v1, v2 = fibonacci(Int(n.value - 1)), fibonacci(Int(n.value - 2))
     return Int(v1.value + v2.value)
-
-
-@registry.register(registry.FunctionType.PREDICATE)
-def transition_monoid(fa: FiniteAutomaton) -> None:
-    if not fa.is_deterministic:
-        return
-
-
-@registry.register(registry.FunctionType.PREDICATE)
-def monoid(fa: FiniteAutomaton) -> None:
-    if not fa.is_deterministic:
-        raise Exception('DFA is not detemenistic')
-    return
-

cbe1970ae68eafb122886312fe1f8de32dfc6c4e
pollykon <pollykon@yandex.ru>
2022-11-03 14:09:37 +0300

    Add pumping length without some functions

--- functions/pumping_length.py
+++ functions/pumping_length.py
@@ -18,10 +18,11 @@ def find_all(a_str, sub):
 
 
 # @registry.register(registry.FunctionType.REGULAR)
-def PumpingLength(regex: str) -> Int:
+def PumpingLength(regex: str) -> int:
     regex_for_thompson = Regex(regex)
     nfa = Thompson(regex_for_thompson)
-    dfa = minimize(nfa)
+    # преобразование в дка (пока отсутствует)
+    # dfa = minimize(nfa)
     parsed_regex = RegexParser.parse(regex)
     orig_tree = parsed_regex
     n = 0
@@ -29,33 +30,41 @@ def PumpingLength(regex: str) -> Int:
     pumped_flag = False
     pumping_prefixes = set()
     while n < 100:
+        # строим префикс длины n
+        # данный блок с накачкой префиксов готов
         prefixes = FiniteAutomaton.prefix(dfa, n)
         all_prefixes_pumped = True
         for prefix in prefixes:
             tree = orig_tree
             for symbol in prefix:
+                # берем производную по префиксу
                 derivative_brzozovski = DerivativeBrzozovski(symbol)
                 derivative_reg_with_prefix = derivative_brzozovski.get_derivative(tree)
                 derivative_reg_with_prefix_regex = tree_to_regex(derivative_reg_with_prefix)
                 tree = derivative_reg_with_prefix
             comb = combinations(prefix, n)
             infixes_list = set([''.join(i) for i in comb])
+            # перебираем все инфиксы по префиксу
             for infix in infixes_list:
                 if infix != prefix:
                     infix_left = prefix.replace(infix, "", 1)
                 if infix_left != prefix:
                     indexes = list(find_all(prefix, infix))
+                    # накачиваем инфиксы
                     for index in indexes:
                         pumping_part = "(" + infix + ")" + "*"
                         pumping_regex = prefix
                         pumping_regex_inf = pumping_regex.replace(infix, "", 1)
                         pumping_regex = pumping_regex_inf[:index] + pumping_part + pumping_regex_inf[len(infix) + index:]
                         regex_to_check = pumping_regex + derivative_reg_with_prefix_regex
-                        for pumping_prefix in  pumping_prefixes:
+                        # оптимизируем перебор, проверяя, если наш префикс начинается с накачиваемого префикса
+                        for pumping_prefix in pumping_prefixes:
                             if regex_to_check.startswith(pumping_prefix):
                                 pumped_flag = True
                             else:
-                                pumped_flag = ksubset(regex_to_check, parsed_regex)
+                                # проверка на пересечение накачимаевого слова и изначального регулярного выражения (пока отсутствует)
+                                # pumped_flag = ksubset(regex_to_check, parsed_regex)
+                        # добавляем накачиваемый префикс для дальнейшей оптимизации
                         if pumped_flag:
                             pumping_prefixes.add(prefix)
                         else:

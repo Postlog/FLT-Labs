df1e7a239cf33d7bd0c80ecc5c4b72ccb2df29c2
dvpashkevich <dvpashkevich@avito.ru>
2022-11-07 22:18:45 +0300

    Predicates: fix subset

--- functions/subset.py
+++ functions/subset.py
@@ -2,43 +2,31 @@ import networkx as nx
 from itertools import product
 
 from functions import registry
+from functions.determinize import determinize
+from functions.thompson import Thompson
 from models import FiniteAutomaton, Regex
-
-
-# Потом заменим на реальные функции
-class Deps:
-    @staticmethod
-    def determinize(self: FiniteAutomaton) -> FiniteAutomaton:
-        return FiniteAutomaton()
-
-    @staticmethod
-    def glushkov(self: Regex) -> FiniteAutomaton:
-        return FiniteAutomaton()
 
 
 @registry.register(registry.FunctionType.PREDICATE)
 def subset(self: FiniteAutomaton | Regex, other: FiniteAutomaton | Regex) -> bool:
     """Проверить, является ли первый НКА/регулярка подмножеством второго НКА/регулярки."""
-    if not (
-        type(self) == type(other)
-        and (isinstance(self, Regex) or isinstance(self, FiniteAutomaton))
-    ):
+    if type(self) != type(other) or type(self) not in (FiniteAutomaton, Regex):
         raise ValueError(
             f'Ожидались аргументы типов (Regex, Regex) или (FiniteAutomaton, FiniteAutomaton), '
             f'получили: {type(self)} и {type(other)}'
         )
 
     if isinstance(self, Regex):
-        self, other = Deps.glushkov(self), Deps.glushkov(other)
+        self = Thompson(self)
+    if isinstance(other, Regex):
+        other = Thompson(other)
     if not self.is_deterministic:
-        self = Deps.determinize(self)
+        self = determinize(self)
     if not other.is_deterministic:
-        other = Deps.determinize(other)
+        other = determinize(other)
 
     # self and other are DFAs by here
-    if self.input_symbols != other.input_symbols:
-        return False
-
+    self, other = _unify(self, other)
     for (state_a, state_b) in _get_product_reachable_states(self, other):
         if state_a in self.final_states and state_b not in other.final_states:
             return False
@@ -54,7 +42,7 @@ def _get_product_reachable_states(self: FiniteAutomaton, other: FiniteAutomaton)
         [
             ((start_state_a, start_state_b), (transitions_a[symbol], transitions_b[symbol]))
             for (start_state_a, transitions_a), (start_state_b, transitions_b), symbol in
-            product(transitions_a, transitions_b, self.input_symbols,)
+            product(transitions_a, transitions_b, self.input_symbols)
         ]
     )
 
@@ -71,3 +59,36 @@ def _simplify_transitions(transitions: dict[str, dict[str, set[str]]]) -> dict[s
             label: list(states_to)[0] for label, states_to in transitions[state_from].items()
         } for state_from in transitions
     }
+
+
+def _unify(
+    self: FiniteAutomaton, other: FiniteAutomaton
+) -> tuple[FiniteAutomaton, FiniteAutomaton]:
+    # Do not use on NFAs
+    alphabet = {
+        symbol
+        for automaton in (self, other)
+        for state_table in automaton.transitions.values()
+        for symbol in state_table.keys()
+    }
+
+    TRAP = chr(666)
+    for automaton in (self, other):
+        automaton.states.add(TRAP)
+        for state_from, state_table in automaton.transitions.items():
+            for symbol in alphabet:
+                if symbol not in state_table:
+                    _overwrite_transition(automaton, state_from, symbol, TRAP)
+        for symbol in alphabet:
+            _overwrite_transition(automaton, TRAP, symbol, TRAP)
+            automaton.transitions[TRAP][symbol] = {TRAP}
+
+    return self, other
+
+
+def _overwrite_transition(self: FiniteAutomaton, state_from: str, symbol: str, state_to: str):
+    # Do not use on NFAs
+    if state_from in self.transitions:
+        self.transitions[state_from][symbol] = {state_to}
+    else:
+        self.transitions[state_from] = {symbol: {state_to}}
